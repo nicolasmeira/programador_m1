aba7dfdf7bd81d07e563f7341d432c1f85ee7b5c
nicolas_meira <nicolas_meira@SNSOO1EP01VM018.saojose.sc.senai.br>
2015-11-23 11:47:12 -0200

    Criação do listagem

--- includes/Estoque.php
+++ includes/Estoque.php
@@ -9,24 +9,49 @@ function salvar() {
     if(
             isset($_POST['nome']) and
             isset($_POST['valor']) and
+            isset($_POST['dtval']) and
             isset($_POST['qtd'])
     ){
         $nome = $_POST['nome'];
         $valor = $_POST['valor'];
         $qtd = $_POST['qtd'];
-        $SQL = "";
+        $dtval = (isset($_POST['dtval']) ? $_POST['dtval'] : "");
+        $SQL = "INSERT INTO produto (nome,valor,quantidade,data_de_validade) values (:nome, :valor, :qtd, :dtval);";
         $preparo = conexao()->prepare($SQL);
-        //$preparo->bindValue("", $nome);
-        //$preparo->bindValue("", $valor);
-        //$preparo->bindValue("", $qtd);
+        $preparo->bindValue("nome", $nome);
+        $preparo->bindValue("valor", $valor);
+        $preparo->bindValue("qtd", $qtd);
+        $preparo->bindValue("dtval", $dtval);
         $preparo->execute();
         if($preparo->rowCount()==1){
             echo"Sucesso!";
         }else{
             echo"Erro!";
         
-        }
-       
+        } 
+        
     }
 } 
 
+function excluir () {
+    $SQL = "DELETE FROM 'produtos' WHERE 'id'=:id";
+    $preparo = conexao()->prepare($SQL);
+    $preparo->bindValue(":id", $_GET['excluir']);
+    $preparo->execute();
+}
+
+function listar() {
+    $SQL = "SELECT * FROM produto WHERE 1;";
+    $preparo = conexao()->prepare($SQL);
+    $preparo->execute();
+    while ($linha = $preparo->fetch(PDO::FETCH_ASSOC))   {
+        echo "<tr>";
+        echo "<td><a value='excluir' href='?excluir=".$linha['idProduto']."'>Excluir</a></td>";
+        echo "<td>" .$linha['idProduto']."</td>";
+        echo "<td>" .$linha['nome']."</td>";
+        echo "<td>".$linha['valor']."</td>";
+        echo "<td>".$linha['quantidade']."</td>";
+        echo "<td>".$linha['data_de_validade']."</td>";
+        echo "<tr>";
+    }
+}
---
+++ listagem.php
@@ -0,0 +1,27 @@
+<?php
+        include './includes/global.php';
+        include './includes/Estoque.php';
+        $title = "Controle de Estoque";
+        include './template/header.php';
+?>
+<div class="center">
+    <table style="width: 100%;">
+        <tr style="background: #CCC;">
+        <th></th>
+        <th>Id</th>
+        <th>Nome</th>
+        <th>Valor</th>
+        <th>Quantidade</th>
+        <th>Data de Validade</th>            
+        </tr>
+    <?php    
+    
+    listar(); 
+    excluir();
+    ?>
+    </table>
+</div>
+
+<?php
+    include './template/footer.php';    
+?>
